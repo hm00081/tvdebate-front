8e16062a46e968a9be3ae4e4b4f317ebd1cd79b2
hm00081 <hm00081@naver.com>
2022-11-24 12:01:54 +0900

    Updates

---
+++ precache-manifest.49254fee9dbc2711d571ef335e3b8aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15bcdec8a51ad748888ed8bea295cb5",
+    "url": "/DebateVis/index.html"
+  },
+  {
+    "revision": "54e204f8973a0f96ebcc",
+    "url": "/DebateVis/static/css/2.ac35b2ca.chunk.css"
+  },
+  {
+    "revision": "1b43dca4bc5e71fcc3ea",
+    "url": "/DebateVis/static/css/main.8d759e1e.chunk.css"
+  },
+  {
+    "revision": "54e204f8973a0f96ebcc",
+    "url": "/DebateVis/static/js/2.652ff5e8.chunk.js"
+  },
+  {
+    "revision": "f6b7eb5a24d3bc6c2ac0849d844f5527",
+    "url": "/DebateVis/static/js/2.652ff5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b43dca4bc5e71fcc3ea",
+    "url": "/DebateVis/static/js/main.d0634d49.chunk.js"
+  },
+  {
+    "revision": "8bbc939d9df573e5a744",
+    "url": "/DebateVis/static/js/runtime-main.6eb3a097.js"
+  }
+]);
