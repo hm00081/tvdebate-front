de745fcccd4c5a0ee3edf00947d76fb554cf8ef9
문광혁 <moonkwanghyuk@mungwanghyeog-ui-MacBookAir.local>
2023-09-07 16:53:00 +0900

    Updates

---
+++ precache-manifest.8f6f04bb41f81de0a83f6be8876b2cf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e83b23ea0812faecc743b3cd258452",
+    "url": "/tvdebate-front/index.html"
+  },
+  {
+    "revision": "2e18333d8028fa0d5999",
+    "url": "/tvdebate-front/static/css/2.ac35b2ca.chunk.css"
+  },
+  {
+    "revision": "17e23b73eda11cb5030d",
+    "url": "/tvdebate-front/static/css/main.1ddc2e8b.chunk.css"
+  },
+  {
+    "revision": "2e18333d8028fa0d5999",
+    "url": "/tvdebate-front/static/js/2.3339df89.chunk.js"
+  },
+  {
+    "revision": "ac24a00c159204e8208fee888436524c",
+    "url": "/tvdebate-front/static/js/2.3339df89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e23b73eda11cb5030d",
+    "url": "/tvdebate-front/static/js/main.dae4526d.chunk.js"
+  },
+  {
+    "revision": "bbe0e848d642258caad1",
+    "url": "/tvdebate-front/static/js/runtime-main.610ea4bd.js"
+  }
+]);
